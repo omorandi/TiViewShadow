2cc825d320fb4b030f2f00dbb284c95fbeb8f1cf
Javier Rayon <javier@criteriastudio.com>
2012-04-18 16:51:43 +0200

    Retina support
    Added support for retina displays, detecting the scale property of
    mainScreen object.

--- example/app.js
+++ example/app.js
@@ -1,5 +1,22 @@
+require('ti.viewshadow');
+
 var win = Ti.UI.createWindow({backgroundColor:'#fff'});
- 
+
+var containerView = Ti.UI.createView({
+
+    top:10,
+    width:300,
+    height:400,
+    borderRadius:20,
+    backgroundColor:'#eee',
+    shadow:{
+        shadowRadius:5,
+        shadowOpacity:1,
+        shadowOffset:{x:5, y:5}
+    }
+
+});
+
 var view = Ti.UI.createView({
     height:100,
     width:100,
@@ -8,27 +25,40 @@ var view = Ti.UI.createView({
 	borderRadius:20,
     backgroundColor:"green",
     shadow:{
-        shadowRadius:2,
+        shadowRadius:10,
         shadowOpacity:0.5,
-        shadowOffset:{x:4, y:4},
+        shadowOffset:{x:5, y:10}
     }
 })
- 
+
+
 var btn = Ti.UI.createButton({
-    title:'Button with shadow',
+    title:'Show/hide view',
     height:60,
     width:200,
     left:60,
-    top:160
+    top:160,
+    borderRadius:10,
+    backgroundImage:'transparent',
+    backgroundColor:'#999'
 });
  
 btn.setShadow({
     shadowRadius:10,
     shadowOpacity:0.5,
-    shadowOffset:{x:5, y:10}
+    shadowOffset:{x:5, y:10},
+	shadowColor:'#393'
 });
 
+btn.addEventListener('click', function(){
 
+    containerView.hide();
+    
+    setTimeout(function(){
+        containerView.show();
+    }, 200);
+
+});
 
 var lbl = Ti.UI.createLabel({
     text:'Shadow #00f',
@@ -43,16 +73,28 @@ var lbl = Ti.UI.createLabel({
 });
  
 lbl.setShadow({
-    shadowRadius:3,
+    shadowRadius:5,
     shadowOpacity:1,
     shadowOffset:{x:0, y:0},
-	shadowColor:'#00f'
+	shadowColor:'#f00'
 });
 
 
-win.add(view);
- 
+containerView.add(view);
+containerView.add(btn);
+containerView.add(lbl);
+win.add(containerView);
+
+/*win.add(view);
 win.add(btn);
-win.add(lbl);
+win.add(lbl);*/
+
+containerView.setShadow({
+    shadowRadius:10,
+    shadowOpacity:0.5,
+    shadowOffset:{x:5, y:10},
+	shadowColor:'#393'
+});
+
 
 win.open();
